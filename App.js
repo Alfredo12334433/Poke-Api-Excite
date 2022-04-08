012a20a325df43daf3dbfbfba63fa40bb0a7342c
Alfredo12334433 <84856847+Alfredo12334433@users.noreply.github.com>
2022-04-08 15:57:16 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import type {Node} from 'react';
 import PokeApiAll from './src/Screens/PokeApiAll';
 import PokeApiOne from './src/Screens/PokeApiOne';
  
- // import Drawer from "./src/navigation/Drawer"
+
  
  const App: () => Node = () => {
  
